b854beef8bd5a88f856699736b356a531df7abe5
daebecodin <136534615+daebecodin@users.noreply.github.com>
2025-06-30 19:12:47 -0700

    extra

---
+++ src/main/java/com/daebecodin/springaimcpragstudybudydemo/QuizQuestion.java
@@ -0,0 +1,4 @@
+package com.daebecodin.springaimcpragstudybudydemo;
+
+public record QuizQuestion(String Question, String answerText, String answerLetter) {
+}
---
+++ src/main/java/com/daebecodin/springaimcpragstudybudydemo/QuizQuestions.java
@@ -0,0 +1,6 @@
+package com.daebecodin.springaimcpragstudybudydemo;
+
+import java.util.List;
+
+public record QuizQuestions(List<QuizQuestion> quizQuestions) {
+}
